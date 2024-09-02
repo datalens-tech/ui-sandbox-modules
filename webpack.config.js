ed029c24dbe000759ba7d19f8bfff56b000818f1
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-09-02 19:34:21 +0300

    Additional libs in the sandbox

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        '@gravity-ui/date-utils/v2.3.0': {
+            import: '@gravity-ui/date-utils.2.3.0',
+        },
+        '@gravity-ui/date-utils/v2.5.3': {
+            import: '@gravity-ui/date-utils.2.5.3',
+        }
+    },
+    mode: 'production',
+    output: {
+        globalObject: 'this',
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist/libs'),
+        chunkFormat: 'commonjs',
+        library: {
+            type: 'global',
+        },
+    },
+};
