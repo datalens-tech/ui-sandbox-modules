691c3f854d47980dd8cf113f4ff2879376569bba
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-11-22 18:58:18 +0300

    Add dom-api module

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const entries = ['d3-chord/v3.0.1', 'd3/v7.9.0'];
+const entries = ['d3-chord/v3.0.1', 'd3/v7.9.0', 'dom-api'];
 
 module.exports = {
     context: __dirname,
