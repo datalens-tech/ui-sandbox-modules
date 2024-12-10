fc895086910b3e63a6e4a689d67e07f9fa5d88a8
Irina V. Kuzmina <kuzmadom@yandex-team.ru>
2024-12-10 18:11:34 +0300

    Fix style setProperty

--- src/entries/dom-api.js
+++ src/entries/dom-api.js
@@ -1,3 +1,8 @@
 import {document as lightJsDocument} from '@litejs/dom';
+import {CSSStyleDeclaration} from '@litejs/dom/css';
+
+CSSStyleDeclaration.prototype.setProperty = function(key, value) {
+	this[key] = value;
+};
 
 export const document = lightJsDocument;
